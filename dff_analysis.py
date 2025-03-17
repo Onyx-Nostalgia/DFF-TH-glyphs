84e7afe891e5c619e834263e0a9274c16c7e0a0a
Onyx-Nostalgia <nostalgia.onyx@gmail.com>
2025-03-17 18:33:26 +0700

    Add DFF analysis script and documentation files

---
+++ dff_analysis.py
@@ -0,0 +1,139 @@
+import argparse
+import os
+import struct
+
+from tabulate import tabulate
+
+from unpack_font import FileReader, read_binary
+
+
+def display_image(data, dimensions=(), offset=None, output_file=None):
+    _offset = offset[0]
+    byte_size = len(data)  # Calculate byte size of the value
+    hex_value = " ".join(f"{b:02X}" for b in data)  # Convert value to hex string
+    _offset += byte_size
+
+    if output_file:
+        # Reformat hex value to be multiline based on image width and height
+        w, h = dimensions
+        hex_lines = []
+        hex_values = hex_value.strip().split(" ")
+        for i in range(0, h):
+            hex_lines.append("".join(hex_values[i * w : (i + 1) * w]))
+
+        folder_path = os.path.dirname(output_file)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        with open(output_file + "_img.txt", "w") as f:
+            f.write("\n".join(hex_lines))
+
+    offset[0] = _offset
+
+
+def display(logs, offset=None, output_file=None):
+    table = []
+    _offset = offset[0]
+    for name, value in logs:
+        try:
+            byte_size = len(
+                struct.pack("i", value)
+            )  # Calculate byte size of the integer
+            hex_value = " ".join(
+                f"{b:02X}" for b in struct.pack("i", value)
+            )  # Convert value to hex string
+        except struct.error:
+            byte_size = len(value)  # Calculate byte size of the integer
+            hex_value = " ".join(
+                f"{b:02X}" for b in value
+            )  # Convert value to hex string
+
+        if len(hex_value) > 24:
+            hex_value = f"{hex_value[:24]}...{hex_value[-6:]}"
+            value = f"{str(hex_value[:24])}...{str(hex_value[-6:])}"
+
+        end_offset = _offset + byte_size - 1
+        table.append(
+            [name, f"{_offset}-{end_offset}", byte_size, hex_value, f"{value}"]
+        )
+        _offset += byte_size
+
+    table_str = tabulate(
+        table,
+        headers=["Name", "Offset", "Byte Size", "Hex", "Value"],
+        tablefmt="grid",
+    )
+
+    if output_file:
+        folder_path = os.path.dirname(output_file)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        with open(output_file, "w") as f:
+            f.write(table_str)
+
+    offset[0] = _offset
+
+
+def dff_analysis(path):
+    filename = os.path.splitext(os.path.basename(path))[0]
+    new_folder_name = f"docs/DFF binary structure/{filename}"
+    data = read_binary(path)
+    reader = FileReader(data)
+
+    logs = [
+        ("magic", reader.read_int32()),
+        ("ascii_num", reader.read_int32()),
+        ("glyph_num", reader.read_int32()),
+    ]
+    offset = [0]
+    display(logs, offset, os.path.join(new_folder_name, "1_Head.txt"))
+
+    # ASCII map glyph
+    ascii_map = []
+    for i in range(logs[1][1]):
+        ascii_map += [(f"glyph_id of ascii_{i}", reader.read_byte())]
+    display(ascii_map, offset, os.path.join(new_folder_name, "2_ASCII.txt"))
+
+    # glyph
+    glyphs = []
+    for i in range(logs[2][1]):
+        glyphs += [
+            (f"glyph_{i}(x_start)", reader.read_word()),
+            (f"glyph_{i}(y_start)", reader.read_word()),
+            (f"glyph_{i}(x_end)", reader.read_word()),
+            (f"glyph_{i}(y_end)", reader.read_word()),
+            (f"glyph_{i}(vertical_offset)", reader.read_word()),
+            (f"glyph_{i}(base_y)", reader.read_word()),
+        ]
+
+    display(glyphs, offset, os.path.join(new_folder_name, "3_Glyph.txt"))
+    unk = reader.read_int32()
+    img_width = reader.read_int32()
+    img_height = reader.read_int32()
+    image = reader.read(img_width * img_height)
+    display(
+        [
+            ("unknown???", unk),
+            ("img_width", img_width),
+            ("img_height", img_height),
+            ("glyph_image", image),
+        ],
+        offset,
+        os.path.join(new_folder_name, "4_Image_metadata.txt"),
+    )
+    display_image(
+        image,
+        (img_width, img_height),
+        offset,
+        os.path.join(new_folder_name, "5_Image.txt"),
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="display DFF file structure table")
+    parser.add_argument("path", type=str, help="(.dff) DFF file path")
+    args = parser.parse_args()
+
+    if not args.path.lower().endswith(".dff"):
+        parser.error("The file must have a .dff extension")
+
+    dff_analysis(args.path)
--- unpack_font.py
+++ unpack_font.py
@@ -1,197 +1,192 @@
-'''
-Python 3
-'''
-
-import os
-import sys
+import argparse
 import json
 import struct
-import argparse
 
+##############################################################
 
-##############################################################	
 
 def read_binary(path):
-	with open(path, "rb") as f:
-		return f.read()
-	return False
-	
+    with open(path, "rb") as f:
+        return f.read()
+    return False
+
+
 def save_binary(path, data):
-	with open(path, "wb") as f:
-		return f.write(data)
-	return False
-	
-##############################################################	
+    with open(path, "wb") as f:
+        return f.write(data)
+    return False
+
+
+##############################################################
+
 
 class FileWriter:
 
-	def __init__(self):
-		self.data = b''
+    def __init__(self):
+        self.data = b""
 
-	def write(self, data):
-		self.data += data
-		
-	def write_dword(self, data):
-		self.data += struct.pack('<I', data)
-		
-	def write_byte(self, data):
-		self.data += struct.pack('B', data)
+    def write(self, data):
+        self.data += data
 
-	def write_word(self, data):
-		self.data += struct.pack('H', data)
+    def write_dword(self, data):
+        self.data += struct.pack("<I", data)
 
-##############################################################	
+    def write_byte(self, data):
+        self.data += struct.pack("B", data)
+
+    def write_word(self, data):
+        self.data += struct.pack("H", data)
+
+
+##############################################################
+
 
 class FileReader:
 
-	def __init__(self, data):
-		self.offset = 0
-		self.data = data
-		
-	def skip(self, size):
-		self.offset += size
-		
-	def read(self, size):
-		out = self.data[self.offset : self.offset + size]
-		self.skip(size)
-		return out
-		
-	def read_int32(self):
-		return struct.unpack('<I', self.read(4))[0]
-	
-	def read_byte(self):
-		return struct.unpack('B', self.read(1))[0]
+    def __init__(self, data):
+        self.offset = 0
+        self.data = data
 
-	def read_word(self):
-		return struct.unpack('H', self.read(2))[0]
+    def skip(self, size):
+        self.offset += size
 
-	def read_sign(self):
-		return struct.unpack('<4s', self.read(4))[0]
+    def read(self, size):
+        out = self.data[self.offset : self.offset + size]
+        self.skip(size)
+        return out
+
+    def read_int32(self):
+        return struct.unpack("<I", self.read(4))[0]
+
+    def read_byte(self):
+        return struct.unpack("B", self.read(1))[0]
+
+    def read_word(self):
+        return struct.unpack("H", self.read(2))[0]
+
+    def read_sign(self):
+        return struct.unpack("<4s", self.read(4))[0]
 
 
 class Coord:
 
-	def __init__(self, x_start, y_start, x_end, y_end, base_x, base_y):
-		self.x_start = x_start
-		self.y_start = y_start
-		self.x_end = x_end
-		self.y_end = y_end
-		self.base_x = base_x
-		self.base_y = base_y
-		
-	@staticmethod
-	def read(reader):
-		x_start = reader.read_word()
-		y_start = reader.read_word()
-		x_end = reader.read_word()
-		y_end = reader.read_word()
-		base_x = reader.read_word()
-		base_y = reader.read_word()
-		return Coord(x_start, y_start, x_end, y_end, base_x, base_y)
+    def __init__(self, x_start, y_start, x_end, y_end, base_x, base_y):
+        self.x_start = x_start
+        self.y_start = y_start
+        self.x_end = x_end
+        self.y_end = y_end
+        self.base_x = base_x
+        self.base_y = base_y
 
-	def __repr__(self):
-		return f'[{self.x_start}:{self.y_start} {self.x_end}:{self.y_end}]'
+    @staticmethod
+    def read(reader):
+        x_start = reader.read_word()
+        y_start = reader.read_word()
+        x_end = reader.read_word()
+        y_end = reader.read_word()
+        base_x = reader.read_word()  # vertical offset
+        base_y = reader.read_word()  # ???
+        return Coord(x_start, y_start, x_end, y_end, base_x, base_y)
+
+    def __repr__(self):
+        return f"[{self.x_start}:{self.y_start} {self.x_end}:{self.y_end}]"
 
 
+##############################################################
 
-##############################################################	
 
-def BuildBMP(data, width, height):
-	writer = FileWriter()
+def build_bmp(data, width, height):
+    writer = FileWriter()
 
-	# HEAD
-	writer.write(b'\x42\x4D') # sign
-	writer.write_dword(1078 + width * height) # BMP size
-	writer.write_word(0) # reserved
-	writer.write_word(0) # reserved
-	writer.write_dword(1078) # bit offset
+    # HEAD
+    writer.write(b"\x42\x4d")  # sign
+    writer.write_dword(1078 + width * height)  # BMP size
+    writer.write_word(0)  # reserved
+    writer.write_word(0)  # reserved
+    writer.write_dword(1078)  # bit offset
 
-	# INFO
-	writer.write_dword(40) # size
-	writer.write_dword(width)
-	writer.write_dword(height)
-	writer.write_word(1) # planes
-	writer.write_word(8) # bit count
-	writer.write_dword(0) # compression
-	writer.write_dword(width * height) # image size
-	writer.write_dword(3780) # pix per meter
-	writer.write_dword(3780) 
-	writer.write_dword(256) # color used
-	writer.write_dword(256) # color important
+    # INFO
+    writer.write_dword(40)  # size
+    writer.write_dword(width)
+    writer.write_dword(height)
+    writer.write_word(1)  # planes
+    writer.write_word(8)  # bit count
+    writer.write_dword(0)  # compression
+    writer.write_dword(width * height)  # image size
+    writer.write_dword(3780)  # pix per meter
+    writer.write_dword(3780)
+    writer.write_dword(256)  # color used
+    writer.write_dword(256)  # color important
 
-	# PALLETE
-	for i in range(256):
-		for i2 in range(3):
-			writer.write_byte(i)
-		writer.write_byte(0)
+    # PALLETE
+    for i in range(256):
+        for _ in range(3):
+            writer.write_byte(i)
+        writer.write_byte(0)
 
-	# LINES
-	lines = []
+    # LINES
+    lines = []
 
-	for i in range(height):
-		line = data[i*width:i*width+width]
-		lines.append(line)
+    for i in range(height):
+        line = data[i * width : i * width + width]
+        lines.append(line)
 
-	data = b''.join(reversed(lines))
-	writer.write(data)
+    data = b"".join(reversed(lines))
+    writer.write(data)
 
-	return writer.data
+    return writer.data
 
-##############################################################	
 
-def BuildJSON(ascii_map, coord_arr, img_width, img_height):
-	data = dict()
-	data['ascii_map'] = ascii_map
-	data['coord_arr'] = dict()
+##############################################################
 
-	for index in range(len(coord_arr)):
-		item = dict()
-		coord = coord_arr[index]
-		item['x_start'] = coord.x_start
-		item['y_start'] = coord.y_start
-		item['x_end'] = coord.x_end
-		item['y_end'] = coord.y_end
-		item['base_x'] = coord.base_x
-		item['base_y'] = coord.base_y
-		data['coord_arr'][index] = item
-		
-	return json.dumps(data, indent=4)
 
-##############################################################	
+def build_json(ascii_map, coord_arr):
+    data = dict()
+    data["ascii_map"] = ascii_map
+    data["coord_arr"] = dict()
+
+    for index in range(len(coord_arr)):
+        item = dict()
+        coord = coord_arr[index]
+        item["x_start"] = coord.x_start
+        item["y_start"] = coord.y_start
+        item["x_end"] = coord.x_end
+        item["y_end"] = coord.y_end
+        item["base_x"] = coord.base_x
+        item["base_y"] = coord.base_y
+        data["coord_arr"][index] = item
+
+    return json.dumps(data, indent=4)
+
+
+##############################################################
 
 
 def unpack_font(path):
-	data = read_binary(path)
-	reader = FileReader(data)
+    data = read_binary(path)
+    reader = FileReader(data)
 
-	magic = reader.read_int32()
-	map_size = reader.read_int32()
-	coord_num = reader.read_int32()
+    _ = reader.read_int32()  # magic
+    ascii_num = reader.read_int32()
+    glyph_num = reader.read_int32()
 
-	ascii_map = dict()
+    ascii_map = {i: reader.read_byte() for i in range(ascii_num)}
+    coord_arr = {i: Coord.read(reader) for i in range(glyph_num)}
 
-	for i in range(map_size):
-		ascii_map[i] = reader.read_byte()
+    _ = reader.read_int32()  # unk
+    img_width = reader.read_int32()
+    img_height = reader.read_int32()
+    image = reader.read(img_width * img_height)
 
-	coord_arr = dict()
+    bmp = build_bmp(image, img_width, img_height)
+    save_binary(path + ".bmp", bmp)
 
-	for i in range(coord_num):
-		coord_arr[i] = Coord.read(reader)
+    json_list = build_json(ascii_map, coord_arr)
+    save_binary(path + ".json", json_list.encode("utf-8"))
 
-	unk = reader.read_int32()
-	img_width = reader.read_int32()
-	img_height = reader.read_int32()
-	image = reader.read(img_width * img_width)
 
-	bmp = BuildBMP(image, img_width, img_height)
-	save_binary(path + '.bmp', bmp)
-
-	json_list = BuildJSON(ascii_map, coord_arr, img_width, img_height)
-	save_binary(path + '.json', json_list.encode('utf-8'))
-	
-
-if __name__ == '__main__':
-	arg_parser = argparse.ArgumentParser(description='Unpack *.DFF file')
-	arg_parser.add_argument('dff_file_path')
-	args = arg_parser.parse_args()
-	unpack_font(args.dff_file_path)
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(description="Unpack *.DFF file")
+    arg_parser.add_argument("dff_file_path")
+    args = arg_parser.parse_args()
+    unpack_font(args.dff_file_path)
