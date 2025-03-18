df8498327c221e39eaa9fa37d1cea651fb7eba79
Onyx-Nostalgia <nostalgia.onyx@gmail.com>
2025-03-19 00:08:57 +0700

    🌟 feat: Add Thai glyph configuration JSON and script for generating glyph settings

---
+++ tools/create_thai_glyph_config.py
@@ -0,0 +1,55 @@
+import argparse
+import json
+import os
+
+
+def create_thai_glyph_config(font_path, is_replace=False):
+    text = "กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรฤลฦวศษสหฬอฮฯะัาำิีึืฺุู฿เแโใไๅๆ็่้๊๋์ํ"
+
+    font_name = os.path.splitext(os.path.basename(font_path))[0].lower()
+    for s in [" ", "_"]:
+        font_name = font_name.replace(s, "-")
+    json_path = f"config/{font_name}.json"
+    print(f"name: {json_path}")
+    if not is_replace and os.path.exists(json_path):
+        print(
+            "File already exists. If you want to replace the file, please use the '--replace' or '-w' option."
+        )
+        return
+    char_dict = {}
+    for char in text:
+        char_dict[char] = {
+            "adjust_x": 0,
+            "adjust_y": 0,
+            "vertical_offset": 0,
+            "rotate": 0,
+        }
+
+    json_data = json.dumps(char_dict, ensure_ascii=False, indent=4)
+
+    with open(json_path, "w", encoding="utf-8") as json_file:
+        json_file.write(json_data)
+
+    print(f"JSON data has been written to {json_path}")
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser(
+        description="""
+Generate Thai glyph configuration JSON for a given font.
+- python tools/create_thai_glyph_config.py Sriracha-Regular.ttf
+- python tools/create_thai_glyph_config.py Sriracha-Regular.ttf -w
+                                         """
+    )
+    arg_parser.add_argument("font_path")
+    arg_parser.add_argument(
+        "-w",
+        "--overwrite",
+        "--replace",
+        dest="is_replace",
+        action="store_true",
+        help="overwrite file mode",
+        default=False,
+    )
+    args = arg_parser.parse_args()
+    create_thai_glyph_config(args.font_path, args.is_replace)
