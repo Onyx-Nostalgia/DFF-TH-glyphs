f0f5803e3aa06c00b0d18e39a9fc2a6e9dbe4acc
Onyx-Nostalgia <nostalgia.onyx@gmail.com>
2025-03-18 23:45:39 +0700

    🌟 feat: Add scripts for packing fonts and analyzing DFF files, including JSON mapping display

---
+++ script/pack_font.sh
@@ -0,0 +1,5 @@
+python pack_font.py data/Thai/RazNotebook_lin_with_thai_glyphs.bmp data/Thai/RazNotebook_lin_with_thai_glyphs.json
+cp data/Thai/RazNotebook_lin_with_thai_glyphs.dff /path/of/file/Games/psychonauts/WorkResource/Fonts/RazNotebook_lin.dff
+
+python pack_font.py data/Thai/bagel_lin_with_thai_glyphs.bmp data/Thai/bagel_lin_with_thai_glyphs.json  
+cp data/Thai/bagel_lin_with_thai_glyphs.dff /path/of/file/Games/psychonauts/WorkResource/Fonts/bagel_lin.dff
