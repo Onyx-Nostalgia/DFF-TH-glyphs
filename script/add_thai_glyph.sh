6a31b061f0a8ced8ca4aa39fe1f36110dfbef90c
Onyx-Nostalgia <nostalgia.onyx@gmail.com>
2025-03-27 01:38:30 +0700

    ✨ feat: ✈️ Add scripts for adding and packing Thai glyphs for multiple fonts

---
+++ script/add_thai_glyph.sh
@@ -0,0 +1,15 @@
+# RazNotebook_lin
+python add_thai_glyph.py data/English/RazNotebook_lin.dff.bmp data/English/RazNotebook_lin.dff.json Sriracha-Regular.ttf
+printf "👍 Completed: RazNotebook_lin\n"
+
+# bagel_lin
+python add_thai_glyph.py data/English/bagel_lin.dff.bmp data/English/bagel_lin.dff.json Sriracha-Regular.ttf
+printf "👍 Completed: bagel_lin\n"
+
+# Tahoma_lin
+python add_thai_glyph.py data/English/Tahoma_lin.dff.bmp data/English/Tahoma_lin.dff.json Sriracha-Regular.ttf --font-size 18 --config config/sriracha-regular-small.json
+printf "👍 Completed: Tahoma_lin\n"
+
+# Arial_lin
+python add_thai_glyph.py data/English/Arial_lin.dff.bmp data/English/Arial_lin.dff.json Sriracha-Regular.ttf --font-size 16 --config config/sriracha-regular-small.json
+printf "👍 Completed: Arial_lin\n"
--- script/pack_font.sh
+++ script/pack_font.sh
@@ -1,5 +1,19 @@
+# RazNotebook_lin
 python pack_font.py data/Thai/RazNotebook_lin_with_thai_glyphs.bmp data/Thai/RazNotebook_lin_with_thai_glyphs.json
 cp data/Thai/RazNotebook_lin_with_thai_glyphs.dff /path/of/file/Games/psychonauts/WorkResource/Fonts/RazNotebook_lin.dff
+printf "👍 Compiled: RazNotebook_lin.dff\n"
 
+# bagel_lin
 python pack_font.py data/Thai/bagel_lin_with_thai_glyphs.bmp data/Thai/bagel_lin_with_thai_glyphs.json  
 cp data/Thai/bagel_lin_with_thai_glyphs.dff /path/of/file/Games/psychonauts/WorkResource/Fonts/bagel_lin.dff
+printf "👍 Compiled: bagel_lin.dff\n"
+
+# Tahoma_lin
+python pack_font.py data/Thai/Tahoma_lin_with_thai_glyphs.bmp data/Thai/Tahoma_lin_with_thai_glyphs.json  
+cp data/Thai/Tahoma_lin_with_thai_glyphs.dff /path/of/file/Games/psychonauts/WorkResource/Fonts/Tahoma_lin.dff
+printf "👍 Compiled: Tahoma_lin.dff\n"
+
+# Arial_lin
+python pack_font.py data/Thai/Arial_lin_with_thai_glyphs.bmp data/Thai/Arial_lin_with_thai_glyphs.json  
+cp data/Thai/Arial_lin_with_thai_glyphs.dff /path/of/file/Games/psychonauts/WorkResource/Fonts/Arial_lin.dff
+printf "👍 Compiled: Arial_lin.dff\n"
